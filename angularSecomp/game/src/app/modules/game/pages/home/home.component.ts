1aaa0f2bc6965fe8c01ca4ad3956718c17eefff7
burca <rlpelizzon@hotmail.com>
2019-09-24 16:00:43 -0300

    login funcionando

--- angularSecomp/game/src/app/modules/game/pages/home/home.component.ts
+++ angularSecomp/game/src/app/modules/game/pages/home/home.component.ts
@@ -15,11 +15,13 @@ import { GameService } from './../../services/game.service';
 export class HomeComponent implements OnInit {
   public modalLoginIsVisible: boolean = false;
   public loginForm: FormGroup;
+  private isLoading: boolean = false;
 
   constructor(
     private gameService: GameService,
-    private formBuilder: FormBuilder
-  ) {}
+    private formBuilder: FormBuilder,
+    private authService: AuthenticateService,
+  ) { }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
@@ -28,8 +30,47 @@ export class HomeComponent implements OnInit {
     })
   }
 
-  openModal()
-  {
+  openModal() {
     this.modalLoginIsVisible = true;
+  }
+
+  handleLogin(loginForm: FormGroup, isLogin: boolean) {
+    if (loginForm.valid) {
+      const { email, password } = loginForm.getRawValue();
+      this.isLoading = true;
+      if (isLogin) {
+        this.login(email, password);
+      }
+      else {
+        this.createUser(email, password);
+      }
+    }
+  }
+
+  async createUser(email: string, password: string) {
+    try {
+      const { user } = await this.authService.register(email, password);
+      await this.authService.setUserInList(user);
+      this.modalLoginIsVisible = false;
+    } catch (error) {
+      console.log('Deu ruim irmao', error);
+    }
+    finally {
+      this.isLoading = false;
+    }
+  }
+  //é bom dividir as funcoes com responsabilidades, login sempre sera login, mas se for nescessario colocar algo a amsi ao criar um usuario
+  //é facil colocar sem mudar a funcionalidade do login (por isso não colocar um boole tipo, isCreate)
+  async login(email: string, password: string) {
+    try {
+      const { user } = await this.authService.login(email, password);
+      await this.authService.setUserInList(user);
+      this.modalLoginIsVisible = false;
+    } catch (error) {
+      console.log('Deu ruim irmao', error);
+    }
+    finally {
+      this.isLoading = false;
+    }
   }
 }
